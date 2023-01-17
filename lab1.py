ecfa5c808fee1587d69674943ef3ab853c3d0951
AhmadRzKhalili <ahmadrkl2000@gmail.com>
2023-01-17 22:00:27 +0330

    A star fixed

--- lab1.py
+++ lab1.py
@@ -243,12 +243,12 @@ def ucs(graph: Graph, start, goal):
 
     return []
 
-# todo: fix node priority in agenda paths
+
 def a_star(graph: Graph, start, goal):
     agenda = []
     agenda.append([graph.get_heuristic(start, goal), start])
     print(agenda, end="\n\n")
-    
+
     while len(agenda) != 0:
 
 
@@ -260,7 +260,7 @@ def a_star(graph: Graph, start, goal):
             return path[1:]
 
         neighbors = graph.get_connected_nodes(node)
-        
+
         extended_nodes = []
 
         for n in neighbors:
@@ -269,37 +269,23 @@ def a_star(graph: Graph, start, goal):
                 path_copy = path.copy()
                 path_copy.append(n)
 
-                path_length = 0
-                for i in range(1, len(path_copy) - 2):
-                    path_length += graph.get_edge(path_copy[i], path_copy[i + 1]).length
 
-                h = graph.get_edge(path[-1], n).length + graph.get_heuristic(n, goal) + path_length
-                
-                path_copy[0] = h
+                g = 0
+                for i in range(1, len(path_copy) - 1):
+                    g += graph.get_edge(path_copy[i], path_copy[i + 1]).length
+                h = graph.get_heuristic(n, goal)
+
+                f =  h + g
+
+                path_copy[0] = f
                 extended_nodes.append(path_copy)         
 
-                    
-        extended_nodes = extended_nodes[::-1]
-        # print(extended_nodes)
+        for extended_node in extended_nodes:
+            agenda.insert(0, extended_node)       
 
-        if len(agenda) == 0:
-            for extended_node in extended_nodes:
-                agenda.insert(0, extended_node) 
-        else:
-            agenda_copy = agenda.copy()
-            for extended_node in extended_nodes:
-                for i in range(len(agenda)):
-                    path = agenda[i]
 
-                    # heuristic comparison
-                    if path[0] > extended_node[0]:
-                        agenda_copy.insert(i, extended_node)
-            agenda = agenda_copy
-            # agenda.insert(0, extended_node)       
-        
-        
-        # agenda = sorted(agenda, key=lambda x: (x[0], len(x)))
-        # agenda = a_star_sort(agenda)
+        agenda = sorted(agenda, key=lambda x: (x[0]))
+        agenda = a_star_sort(agenda)
 
         print(agenda, end="\n\n")
 
@@ -314,15 +300,18 @@ def a_star_sort(agenda):
         for j in range(0, len(agenda) - i - 1):
 
             # heuristic comparison
-            if agenda[j][0] > agenda[j + 1][0]:
-                agenda[j], agenda[j + 1] = agenda[j + 1], agenda[j]
+            if agenda[j][0] == agenda[j + 1][0]:
 
-
-            elif agenda[j][0] == agenda[j + 1][0]:
-
-                # number of visited node comparsion
+                # number of visited nodes comparison
                 if len(agenda[j]) > len(agenda[j + 1]):
                     agenda[j], agenda[j + 1] = agenda[j + 1], agenda[j]
+
+                elif len(agenda[j]) == len(agenda[j + 1]):
+
+                    # node priority comparsion
+                    if agenda[j] > agenda[j + 1]:
+                        agenda[j], agenda[j + 1] = agenda[j + 1], agenda[j]
+
 
             
     return agenda
@@ -369,7 +358,7 @@ if __name__ == "__main__":
     
     # Use different algorithms here
     # path = beam_search(graph, start, goal, 1)
-    path = ids(graph, start, goal)
+    path = a_star(graph, start, goal)
 
     if path:
         print(path)
@@ -382,7 +371,7 @@ if __name__ == "__main__":
         # Visualization (extra credit)
         try:
             # Change 'algorithm_label' for better labelling
-            algorithm_label = "IDS"
+            algorithm_label = "A_star"
 
             from visualize import draw_graph
             draw_graph(graph, goal=goal, output_name=algorithm_label+" Graph",
